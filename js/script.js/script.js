dbf70cbc9cb06e06e5215b3e4e61d34d9090abf9
hannahapuan <hannah.apuan@du.edu>
2017-05-27 11:27:14 -0600

    added logos--

--- js/script.js/script.js
+++ js/script.js/script.js
@@ -34,3 +34,50 @@ function reverseTimeline(e) {
   tl.reverse();
 }
 
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() { 
+  strokeWeight(1);
+  stroke('#c7a5a1');
+  fill('#eac5b7');
+
+  function vortex(count) {
+    for (var i = 0; i < count; i++) {
+      triangle(i*width/count,0,(i+1)*width/count,0,mouseX,mouseY);
+      triangle(0,i*height/count,0,(i+1)*height/count,mouseX,mouseY);
+      triangle(i*width/count,height,(i+1)*width/count,height,mouseX,mouseY);
+      triangle(width,i*height/count,width,(i+1)*height/count,mouseX,mouseY);
+    }
+  }
+  
+  vortex(100);
+}
+
+var latestKnownScrollY = 0,
+    ticking = false;
+
+function onScroll() {
+	latestKnownScrollY = window.scrollY;
+  requestTick();
+}
+function requestTick() {
+	if(!ticking) {
+		requestAnimationFrame(update);
+	}
+	ticking = true;
+}
+
+function update() {
+  ticking = false;
+  var currentScrollY = latestKnownScrollY;
+  console.log(currentScrollY);
+  $('#shape1').css('transform','translate(0, -' + ( currentScrollY * 0.8 ) + 'px)');
+  $('#shape2').css('transform','translate(0, -' + ( currentScrollY * 0.5 ) + 'px)');
+  $('#shape3').css('transform','translate(0, -' + ( currentScrollY * 0.7 ) + 'px)');
+  
+	
+}
+
+window.addEventListener('scroll', onScroll, false);
