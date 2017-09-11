83d9ef06baa5f66894aa5827decdc1ba7179a544
Hannah Apuan <hannah.apuan@gmail.com>
2017-09-11 15:59:20 -0600

    changed to water

--- testing/stars.js
+++ testing/stars.js
@@ -1,32 +1,107 @@
-(()=>{
-	 let $ = c.getContext("2d"),
-	    h = c.height = window.innerHeight,
-	       w = c.width = window.innerWidth,
-	          random = (n)=>Math.random()*n,
-		     stars = new Array(1000).fill().map(()=>{
-			         return {r: random(w), s: random(0.01), a: random(Math.PI*2)};
-				    });
-	  function loop(){
-		    $.fillStyle="rgba(0,0,0,.1)";
-		      $.fillRect(0,0,w,h);
-		        stars.forEach(e=>{
-				   e.a+=e.s;
-				      $.save();
-				         $.beginPath();
-					    $.translate(w/2, h/2); 
-					       $.rotate(e.a);
-					          $.arc(e.r,e.r,1,0,Math.PI*2);
-						     $.arc(Math.cos(e.a)*e.r + w/2, Math.sin(e.a)*e.r + h/2,1,0,Math.PI*2);
-						        $.closePath();
-						           $.fillStyle = "white";
-						              $.fill();
-						                 $.restore();
-						                   });
-						                     requestAnimationFrame(loop);
-						                      }
-						                       loop();
-						                        window.addEventListener("resize", (e)=>{
-						                          w=c.width=window.innerWidth;
-						                            h=c.height=window.innerHeight;
-						                             });
-						                             })();
+var canvas = document.getElementById('tint');
+			var ctx = canvas.getContext('2d');
+			//var lvl = 1;
+			ctx.fillStyle = '#FFFFFF';
+			var plus = 0;
+			var mouseX = 0;
+			var mouseY = 0;
+							//    var boxX = 0;
+					//var boxY = 0;
+					//var boxSize = 0;
+			//var clicked = false;
+			 //var sqWidth = 0;
+			 //var difficulty = 0;
+			 //var box;
+			 //var color = getRandColor();
+			 setInterval(draw, 50);
+			// setInterval(function(){ tint = 0; }, 18);
+			// var tint = 0;
+			 var effect = 0;
+			 var eX = 0;
+			 var eY = 0;
+			 var distanceX = 0;
+			 var distanceY = 0;
+
+									canvas.addEventListener('click', function (evt) {
+										 var rect = canvas.getBoundingClientRect();
+					mouseY = evt.clientY - rect.top;
+					mouseX = evt.clientX - rect.left;
+					}
+			);
+			//var temp = 0;
+			function draw() {
+					var count = 0;
+					ctx.clearRect(0, 0, canvas.width, canvas.height);
+					size = 5;
+					//var number = 100+p/100;
+					var p = 10;
+					var d = 0;
+					//ctx.fillRect(0, 0, 500, 500);
+
+				 if(effect > 0){
+						 effect = effect - 1;
+				 }
+				 else if(mouseX > 0 && mouseY > 0 && effect == 0){
+								 effect = 20;
+								 eX = mouseX;
+								 eY = mouseY;
+								 mouseX = 0;
+								 mouseY = 0;
+				 }
+
+					for (var y = p + size; y <= canvas.height - size/2; y += size + p) {
+							for (var x = p + size + Math.cos(plus); x <= canvas.width - size/2; x += size + p, count += 1) {
+
+									var a= (eX-x);
+								 var b = eY - y;
+								 d = Math.sqrt((a*a)+(b*b));
+							ctx.beginPath();
+							var dX = x/d;
+							var dY = y/d;
+							ctx.arc(x + Math.cos(plus)*(6+(dX*effect)), y + Math.sin(plus)*(2+(dX*effect)), 1, 0, 2*Math.PI);
+									ctx.fill();
+									plus += Math.PI/16;
+							}
+//                 //plus = plus + .000005;
+
+
+					}
+
+			}
+			function getMouseX(canvas, event){
+					var rect = canvas.getBoundingClientRect();
+					var x = event.clientX - rect.left;
+					return x;
+			}
+
+			function getMouseX(canvas, event){
+					var rect = canvas.getBoundingClientRect();
+					var y = event.clientY - rect.top;
+					return y;
+			}
+//        function lighten(color, luminosity) {
+//
+//            // validate hex string
+//            color = new String(color).replace(/[^0-9a-f]/gi, '');
+//            if (color.length < 6) {
+//                color = color[0] + color[0] + color[1] + color[1] + color[2] + color[2];
+//            }
+//            luminosity = luminosity || 0;
+//
+//            // convert to decimal and change luminosity
+//            var newColor = "#", c, i, black = 0, white = 255;
+//            for (i = 0; i < 3; i++) {
+//                c = parseInt(color.substr(i * 2, 2), 16);
+//                c = Math.round(Math.min(Math.max(black, c + (luminosity * white)), white)).toString(16);
+//                newColor += ("00" + c).substr(c.length);
+//            }
+//            return newColor;
+//        }
+//        function getRandColor() {
+//            var letters = '0123456789ABCDEF'.split('');
+//            var color = '#';
+//            for (var i = 0; i < 6; i++) {
+//                color += letters[Math.floor(Math.random() * 16)];
+//            }
+//            return color;
+//        }
